49d690887826703e350f2a127b7854d5058b0b8f
vokrob <106639488+vokrob@users.noreply.github.com>
2025-02-06 18:07:37 +0700

    Connect Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -60,6 +61,7 @@ dependencies {
     implementation(libs.androidx.foundation)
     implementation(libs.ui.tooling)
     implementation(libs.material)
+    implementation(libs.firebase.database)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
